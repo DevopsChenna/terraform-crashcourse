411149119fa4d4ccc3c9cfe5b34f4cfdc4ad82df
Diego Ferreira <katesclau@gmail.com>
2017-02-18 00:05:34 -0200

    CrashCourse Single Instance

---
+++ variables.tf
@@ -0,0 +1,5 @@
+
+variable "security_group" { default = "sg-b3a031ca" }
+
+variable "key_name" { default = "crashcourse" }
+variable "public_key" { default = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzQBhyomFGGECkPQ+a4WTLZq+Tz2o824dNaXISsjGsbIKD6m9N4QH/jdCDyIiICC52XjTestwFYzuYS8QJ2aYIdUIj1nDWPPqmAtsufY2dzvLul7DTx4DtrQvOHBeWpShszyyRc4mzmQdeFH+eQncNUiFsAQKaYEjKdnpCsaoDYHa1okMyHsLeUo4HI9kR+b0GndWsCFOz7dB63uyAgI0JtY97aYbE7GBkOxh2HIUHXruZFZ2J6fU/zXWwOm1P0+LFP6x04fOHyfdVpsWefMR5koLsCicEvmnjcw8fTRzO0RVPgptEUIUSgy99JyjFYE0Qk/HGmPplRsXqP1UV7mHfQIDAQAB" }
