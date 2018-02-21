55f7ac0eb024c3c3aa9783b7afbc76dbad66703a
devopschenna <chennadevops@gmail.com>
2018-02-21 15:49:36 -0600

    terraform crashcourse of single instance

---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "security_group" { default = "sg-08c8f77f" }
+
+variable "key_name" { default = "crashcourse" }
+variable "public_key" { default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkW2junn1Hl7O8NfttaWybwiPOPdjp44nKHKmIlgxplppkhdq/dro2z5TADZfpgoptcD4lqrpXJO+c758oqAPpPeHWJRsLOzwoc8v54kfzqcoKsy9WNqPX7TXgAnsX7GDzRES1n5xv03Hhuxq2/xoEYCWo3iLKj57PXqE+VOxs7trlaqa67yUEGb4Uj6+1AtccDc46a8LsizsYiEwMnmO1vkRNKrQW78ojIAuCKAOJ/ByAl2ICf2lxKnJ2Qn9lT6iE61RUsFBSaBVRWRdBavD1+eeoIKWtN/N845/FhRIMK51alhoBsBzxhOlKzV8CFWF/LIFwVNyMt90EZznhWXjD" }
